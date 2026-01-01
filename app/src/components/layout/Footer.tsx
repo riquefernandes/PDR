dcdd42e8529e2e553eef1f52c69723f2357e5f01
riquefernandes <henriquefernandesleite@gmail.com>
2026-01-01 10:38:22 -0300

    Centraliza o texto do footer

--- app/src/components/layout/Footer.tsx
+++ app/src/components/layout/Footer.tsx
@@ -2,8 +2,8 @@
 export function Footer() {
   return (
     <footer className="w-full mt-24">
-      <div className="mx-auto max-w-4xl px-8 py-8 border-t border-accents-2">
-        <p className="text-sm text-accents-6">
+      <div className="display-flex justify-center  mx-auto max-w-4xl px-8 py-8 border-t border-accents-2">
+        <p className="text-sm/1 text-center ">
           Desenvolvido com Next.js e ❤️. Inspirado no design da Vercel.
         </p>
       </div>
