659e922ec9289bd311c2934f12dba1ebad04df5c
Henrique <henriquefernandesleite@gmail.com>
2026-05-16 09:40:12 -0300

    fix(config): correct darkMode property in tailwind config

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -2,7 +2,7 @@ import tailwindcssAnimate from "tailwindcss-animate";
 import type { Config } from "tailwindcss"
 
 const config = {
-  darkMode: ["class"],
+  darkMode: "class",
   content: [
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
